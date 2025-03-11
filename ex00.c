6f24feaffafa3372b1c3e63abb26d48e8af8b857
simoo <hamoyisimo@gmail.com>
2025-03-11 12:19:58 +0000

    first ex

---
+++ ex00.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+  int n ;
+  int i ; 
+  printf("enter a number : ");
+  scanf("%d",&n);
+
+  for ( i = n +1 ; i <= n +10; i++)
+  {
+
+    printf("%d",i);
+  }
+  
+}
