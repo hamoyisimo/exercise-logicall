7ace9fd2e5797f41b1be059b6ea2284d3f9f68e1
simoo <hamoyisimo@gmail.com>
2025-04-07 15:24:00 +0100

    learc tab

---
+++ tableau1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int  main ()
+{
+    int t[10];
+    int i ; 
+    for ( i = 0; i < 10; i++)
+    {
+      t[i] = 0;
+    }
+    for ( i = 0; i < 10; i++)
+    {
+      printf("t[%d] = %d\n",i,t[i]);
+    }
+     
+    
+}   
