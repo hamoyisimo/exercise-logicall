74790bf3dd36d7982f951db108396d4b0786a322
simoo <hamoyisimo@gmail.com>
2025-04-08 12:37:08 +0100

    voyelles

---
+++ tab2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int  main ()
+{
+    char  t[6] = {'a' , 'u' , 'o' , 'i' , 'y' , 'e'};
+    int i ; 
+      printf("les voyelles de l'aphabet francaise sont : ");
+      for ( i = 0; i < 6 ; i++)
+        {
+           printf("%c\n",t[i]);
+        }
+}   
